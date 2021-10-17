ef988cbcda9bd11f6065c629a0ed039e26c13389
黃奕文 <gy45213@live.com>
2021-10-17 12:35:48 +0800

    add linear equation with one unknowns. add quadratic equation of one unknowns add  linear equation of two unknowns

--- calculater/main.cpp
+++ calculater/main.cpp
@@ -10,12 +10,15 @@ struct Member {
 	float cc;
 };
 void calculate(Member& ft);
+void calculate(Member& ft, Member& ft1);
 void calculate(float, float, float);
 void calculate_qone(float, float, float);
 int Show_mode(int);
 
 int main(){
 	Member num;
+	Member num1;
+	Member num2;
 	string con;
 	int select;
 	cout << "Welcome, ";
@@ -45,6 +48,16 @@ int main(){
 			break;
 		case 4:
 			// linear equation of two unknowns
+			cout << "Please enter 2 of 1, a,b,c for ax + by + c = 0 " << endl;
+			cout << "a = ";	cin >> num1.a;
+			cout << "b = "; cin >> num1.b;
+			cout << "c = "; cin >> num1.cc;
+			cout << "Please enter 2 of 2, a,b,c for ax + by + c = 0 " << endl;
+			cout << "a = ";	cin >> num2.a;
+			cout << "b = "; cin >> num2.b;
+			cout << "c = "; cin >> num2.cc;
+			calculate(num1, num2);
+
 			break;
 		case 5:
 			// quadratic equation of two unknowns
@@ -91,19 +104,64 @@ void calculate(float aa, float bb, float cc) {
 }
 void calculate_qone(float aa, float bb, float cc) {
 	float x;
-	bb = pow(bb, 2);
-	x = bb - 4 * aa * cc;
+	float x1;
+	float x2;
+	float bb1;
+	bb1 = pow(bb, 2);
+	x = bb1 - 4 * aa * cc;
 	if (x > 0) {
+		//Τㄢぃ龟计
 		cout << "Τㄢぃ龟计" << endl;
+		cout << "X = " << x << endl;
+		x = sqrt(x);
+		x1 = (-bb + x) / 2 * aa;
+		x2 = (-bb - x) / 2 * aa;
+		cout << "x1 = " << x1 <<" , x2 = " << x2 << endl;
+
 	}
 	else if (x == 0) {
 		//ㄢ单龟计
+		cout << "ㄢ单龟计 \n";
+		x1 = -bb / 2 * aa;
+		cout << " x = " << x << endl;
 	}
 	else {
 		//ㄢぃ狡计砿狡
+		cout << "ㄢぃ狡计砿狡 \n";
+		struct answer {
+			float answer_x1;
+			float answer_x2;
+		};
+
+		answer x3;
+		x3.answer_x1 = -bb / 2 * aa;
+		x3.answer_x2 = 4*aa*cc - bb1;
+		x3.answer_x2 = sqrt(x3.answer_x2) / 2 * aa;
+		cout << "x = " << x3.answer_x1 << " + " << x3.answer_x2 << "i" << endl;
+		cout << "x = " << x3.answer_x1 << " - " << x3.answer_x2 << "i" << endl;
+			
 	}
 		
 }
+void calculate(Member& ft, Member& ft1) {
+	float x;
+	float y;
+	x = (ft.b * ft1.cc - ft.cc * ft1.b) / (ft.a * ft1.b - ft.b * ft1.a);
+	y = (ft.a * ft1.cc - ft1.a * ft.cc) / (ft1.a * ft.b - ft.a * ft1.b);
+	cout << "Answer is : " << endl;
+	cout << " X = " << x << endl;
+	cout << " Y = " << y << endl;
+	//	cout << "Please enter 2 of 1, a,b,c for ax + by + c = 0 " << endl;
+	//cout << "a = ";	cin >> num1.a;
+	//cout << "b = "; cin >> num1.b;
+	//cout << "c = "; cin >> num1.cc;
+	//cout << "Please enter 2 of 2, a,b,c for ax + by + c = 0 " << endl;
+	//cout << "a = ";	cin >> num2.a;
+	//cout << "b = "; cin >> num2.b;
+	//cout << "c = "; cin >> num2.cc;
+}
+
+
 
 
 int Show_mode(int) {
